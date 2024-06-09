d889f1d42026fd71592b39adb573009b190b98b5
igot-apps <aagortey@gmail.com>
2024-06-09 22:23:53 +0000

    http2 disable

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ const initializeBrowser = async () => {
         "--no-sandbox",
         "--single-process",
         "--no-zygote",
-        "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        "--disable-http2"
       ],
       executablePath: process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
